4943c46a3042ba2b5704a622e8ecbc0cca98083e
Dhanashree <92945568+Dhanashree-31@users.noreply.github.com>
2023-03-07 18:07:07 +0530

    Update extract_zip.py
    updated unzip coed

--- scripts/preprocess/extract_zip.py
+++ scripts/preprocess/extract_zip.py
@@ -2,5 +2,6 @@ import zipfile
 
 
 def extract_zip(base_location, target_location):
-    with zipfile.ZipFile(base_location, "r") as zip_ref:
-        zip_ref.extractall(target_location)
+    zip_ref = zipfile.ZipFile(base_location,'r')
+    zip_ref.extractall(target_location)
+    zip_ref.close()
